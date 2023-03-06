7d2b25389c2927076e857e31c62f14e242b9a28b
namraaslam <namra.aslam@ucalgary.ca>
2023-03-06 12:01:35 -0500

    minor refcatoring, debugging, implement form submission, validation w/ ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,28 +8,6 @@
 $(document).ready(function() {
 
 const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
 ]
 
 const renderTweets = function(tweets) {
@@ -42,6 +20,7 @@ for (const tweet of tweets) {
 }
 
 const createTweetElement = function(tweet) {
+  const time = timeago.format(tweet.created_at);
   return $(`
     <article>
       <div class="tweet-creater">
@@ -58,7 +37,7 @@ const createTweetElement = function(tweet) {
       </div>
       <div class="time-created">
         <div>
-          <span>${tweet.created_at}</span>
+          <span>${time}</span>
         </div>
         <div>
           <span>
@@ -73,8 +52,44 @@ const createTweetElement = function(tweet) {
 };
 renderTweets(data);
 
+const tweetForm = $('.tweet-form');
+tweetForm.on('submit', function(event) {
+  event.preventDefault();
+  const serializedData = $(this).serialize();
+  const tweetText = $(`#tweet-text`).val();
+  console.log(tweetText);
 
+  const length = tweetText.trim().length;
+  if (length === 0) {
+    alert("Empty tweets are not accepted.");
+  } else if (length > 140) {
+    alert("140 characters exceeded.");
+  } else {
+    $.post('/tweets', serializedData)
+      .done(function(data) {
+        loadTweets();
+      })
+      .fail(function(error) {
+        console.error(error);
+      });
+  }
+});
 
+const loadTweets = () => {
+  $.ajax({
+    url: "/tweets",
+    type: "GET",
+    dataType: "json",
+    success: function(res) {
+      renderTweets(res);
+    },
+    error: function(error) {
+      console.error(error);
+    }
+  });
+};
+
+loadTweets();
 
 });
 
